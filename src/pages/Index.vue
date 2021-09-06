2a138f7cfbceff018ebd2c7cf18b7ba7d9fa15a9
Paulo Thiago Seixas Serrao <paulo.serrao@sidia.com>
2021-09-06 10:42:32 -0400

    initial template

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,17 +1,60 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
-  </q-page>
+  <div :class="$q.screen.lt.md ? 'column' : 'row'">
+    <div class="col-4 flex itens-center">
+      <div class="row">
+        <div class="col">
+          <h4 class="text-h4 ubuntu-font text-weight-bolder">
+            Faça a Gestão da sua comunidade de uma forma simples e completa
+          </h4>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col">
+          <p class="text-h5 gray-ligth">
+            Multiplicame é uma plataforma que possui tudo o que você precisa
+            para trabalhar senso de pertencimento e produtividade de membros.
+          </p>
+        </div>
+      </div>
+
+      <div class="row desktop-only flex self-end">
+        <div class="col">
+          <q-btn
+            color="primary-dark"
+            icon="login"
+            size="md"
+            padding="md"
+            label="TODAS AS FUNCIONALIDADES"
+            class="q-px-md"
+            to="/functionalities"
+          />
+        </div>
+      </div>
+    </div>
+
+    <div class="col flex justify-center">
+      <img
+        alt="Main image"
+        src="~assets/image_2.svg"
+        :style="$q.screen.lt.sm ? { height: '230px' } : { height: '570px' }"
+      />
+    </div>
+
+    <q-btn
+      color="primary-dark"
+      icon="east"
+      class="mobile-only q-mt-md"
+      label="TODAS AS FUNCIONALIDADES"
+      to="/functionalities"
+    />
+  </div>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'PageIndex'
-})
+  name: "PageIndex",
+});
 </script>
